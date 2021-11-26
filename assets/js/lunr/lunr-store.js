38360a195af857411d06feac010ea71184bc5a7c
ManticSic <darius.weber@posteo.de>
2021-11-26 21:47:05 +0000

    Deploying to gh-pages from @ ManticSic/jekyll-page@8e46564805d0b68638fa01b76cb564830baeb647 🚀

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Home Server",
+        "excerpt":"Here you should find the story why I decided to build a custom server and what trouble I had while building ist.  Unfortunately it’s not finished yet… that’s why you have to read this small message.  ","categories": [],
+        "tags": [],
+        "url": "/projects/home-server/",
+        "teaser": null
+      }]
